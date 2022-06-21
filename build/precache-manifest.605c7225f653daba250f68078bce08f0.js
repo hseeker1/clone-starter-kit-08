5b94ca9ab463509d106ccea65eb7eccb2f628666
hseeker1 <seungoh.han@gmail.com>
2022-06-21 10:20:42 +0900

    local-server upload test

---
+++ build/precache-manifest.605c7225f653daba250f68078bce08f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca2e083d10715712d7811851c07a6322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c83d16f6eeca07bc94f6",
+    "url": "/static/css/main.3bc3c130.chunk.css"
+  },
+  {
+    "revision": "2ed221e88d106eeea028",
+    "url": "/static/js/2.c2d6362a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c2d6362a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83d16f6eeca07bc94f6",
+    "url": "/static/js/main.3adf8e65.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.3adf8e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
